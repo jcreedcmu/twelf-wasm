24a40dc3f116e54547c4839d26e4cc7d3ea48a81
Rob Simmons <robsimmons@gmail.com>
2024-03-12 14:52:25 -0400

    Clean up diff

--- src/twelf-mode.ts
+++ src/twelf-mode.ts
@@ -1,8 +1,4 @@
-import {
-  defaultHighlightStyle,
-  HighlightStyle,
-  StreamLanguage,
-} from "@codemirror/language";
+import { defaultHighlightStyle, HighlightStyle, StreamLanguage } from "@codemirror/language";
 import { tags as t } from "@lezer/highlight";
 import { mkStreamParser } from "./twelf-highlight/tokenizer-codemirror-adapter";
 import { twelfTokenizer } from "./twelf-highlight/twelf-tokenizer";
@@ -11,8 +7,8 @@ export const twelfHighlightStyle = HighlightStyle.define([
   ...defaultHighlightStyle.specs,
   { tag: t.variableName, color: "#0a0" },
   { tag: t.atom, color: "#000" },
-  { tag: t.comment, color: "#777", fontStyle: "italic" },
-]);
+  { tag: t.comment, color: "#777", fontStyle: "italic" }
+])
 
 export const twelfLanguage = StreamLanguage.define(
   mkStreamParser(twelfTokenizer, {
